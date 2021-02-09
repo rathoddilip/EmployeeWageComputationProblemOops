b3a05b4910948d64c0dc153949191aac8f4a3921
diliprathod32 <diliprathod32@gmail.com>
2021-02-10 02:52:41 +0530

    [Dilip] PartTime

--- EmployeeWageComputationOops/Program.cs
+++ EmployeeWageComputationOops/Program.cs
@@ -4,22 +4,29 @@ namespace EmployeeWageComputationOops
 {
 	public class Program
 	{
-		const int PRESENT = 1;
+		const int EMP_FULL_TIME = 1;
+		const int EMP_PART_TIME = 2;
 		const int WAGE_PER_HOUR = 20;
 		const int FULL_DAY_HOUR = 8;
+		const int PART_TIME_HOUR = 4;
 		public static void Main(String[] args)
 		{
 			Console.WriteLine("Welcome to employee wage computation!");
 
 			Random rand = new Random();
-			int attendance = rand.Next(0, 2);
-			int dailywage = 0;
+			int attendance = rand.Next(0, 3);
+			int workingHours = 0;
+			int dailywage;
 
-			if (attendance == PRESENT)
+			if (attendance == EMP_FULL_TIME)
 			{
-				dailywage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-
+				workingHours = FULL_DAY_HOUR;
 			}
+			else if (attendance == EMP_PART_TIME)
+			{
+				workingHours = PART_TIME_HOUR;
+			}
+			dailywage = WAGE_PER_HOUR * workingHours;
 			Console.WriteLine($"Daily Wage : {dailywage}");
 		}
 	}
