402d1bd854455f0bbf87837f678169495bbbc9b9
diliprathod32 <diliprathod32@gmail.com>
2021-02-10 03:15:19 +0530

    [Dilip] InstancevariableAndEmpWageBuilder

---
+++ EmployeeWageComputationOops/EmpWageBuilder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputationOops
+{
+	class EmpWageBuilder
+	{
+		const int FULL_DAY_HOUR = 8;
+		const int PART_TIME_HOUR = 4;
+		const int EMP_FULL_TIME = 1;
+		const int EMP_PART_TIME = 2;
+		readonly string companyName;
+		readonly int wagePerHour;
+		readonly int maxWorkingDays;
+		readonly int maxWorkingHours;
+		int wagesPerMonth = 0;
+		public EmpWageBuilder(string companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours)
+		{
+			this.companyName = companyName;
+			this.wagePerHour = wagePerHour;
+			this.maxWorkingDays = maxWorkingDays;
+			this.maxWorkingHours = maxWorkingHours;
+		}
+		public void ComputeWage()
+		{
+			int workingHours = 0;
+			int workingDays = 0;
+
+			Random rand = new Random();
+
+			while (workingHours < maxWorkingHours && workingDays < maxWorkingDays)
+			{
+				int attendance = rand.Next(0, 3);
+				int hoursWorked = 0;
+				switch (attendance)
+				{
+					case EMP_FULL_TIME:
+						workingDays += 1;
+						hoursWorked = FULL_DAY_HOUR;
+						break;
+					case EMP_PART_TIME:
+						workingDays += 1;
+						hoursWorked = PART_TIME_HOUR;
+						break;
+					default:
+						break;
+				}//end Switch
+
+				workingHours += hoursWorked;
+				wagesPerMonth += (wagePerHour * workingHours);
+
+			}//end while
+
+		}
+		public override string ToString()
+		{
+			return $"Wages for the company {companyName} for the month : {wagesPerMonth}";
+		}
+	}
+}
--- EmployeeWageComputationOops/Program.cs
+++ EmployeeWageComputationOops/Program.cs
@@ -4,46 +4,15 @@ namespace EmployeeWageComputationOops
 {
 	public class Program
 	{
-		const int FULL_DAY_HOUR = 8;
-		const int PART_TIME_HOUR = 4;
-		const int EMP_FULL_TIME = 1;
-		const int EMP_PART_TIME = 2;
 
-		static void ComputeWage(string companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours)
-		{
-			int workingHours = 0;
-			int workingDays = 0;
-			int wagesPerMonth = 0;
-			Random rand = new Random();
-
-			while (workingHours < maxWorkingHours && workingDays < maxWorkingDays)
-			{
-				int attendance = rand.Next(0, 3);
-				int hoursWorked = 0;
-				switch (attendance)
-				{
-					case EMP_FULL_TIME:
-						workingDays += 1;
-						hoursWorked = FULL_DAY_HOUR;
-						break;
-					case EMP_PART_TIME:
-						workingDays += 1;
-						hoursWorked = PART_TIME_HOUR;
-						break;
-					default:
-						break;
-				}//end Switch
-
-				workingHours += hoursWorked;
-				wagesPerMonth += (wagePerHour * workingHours);
-
-			}//end while
-			Console.WriteLine($"Wages for the company {companyName} for the month : {wagesPerMonth}");
-		}
 		public static void Main(String[] args)
 		{
-			ComputeWage("IBM", 1000, 20, 100);
-			ComputeWage("Infosys", 100, 30, 150);
+			EmpWageBuilder TCS = new EmpWageBuilder("IBM", 1000, 20, 100);
+			TCS.ComputeWage();
+			Console.WriteLine(TCS.ToString());
+			EmpWageBuilder RupakAndSons = new EmpWageBuilder("Infosys", 100, 30, 150);
+			RupakAndSons.ComputeWage();
+			Console.WriteLine(RupakAndSons.ToString());
 		}
 	}
 }
