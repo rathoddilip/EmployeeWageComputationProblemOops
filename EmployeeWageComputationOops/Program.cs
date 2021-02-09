f444be1f8feebcba96c7910e7803544b330bf0bf
diliprathod32 <diliprathod32@gmail.com>
2021-02-10 02:40:59 +0530

    [Dilip] EmployeeAttendence

--- EmployeeWageComputationOops/Program.cs
+++ EmployeeWageComputationOops/Program.cs
@@ -4,9 +4,17 @@ namespace EmployeeWageComputationOops
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to employee wage computation problem");
-        }
-    }
+		const int PRESENT = 1;
+		const int ABSENT = 2;
+		public static void Main(String[] args)
+		{
+			Console.WriteLine("Welcome to employee wage computation!");
+			Random rand = new Random();
+			int attendance = rand.Next(0, 2);
+			if (attendance == PRESENT)
+				Console.WriteLine("Employee Present");
+			else
+				Console.WriteLine("Employee Absent");
+		}
+	}
 }
