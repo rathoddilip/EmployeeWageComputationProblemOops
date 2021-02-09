2458d29d7dc00a952525e6299e146e22c9ed24ef
diliprathod32 <diliprathod32@gmail.com>
2021-02-10 02:57:44 +0530

    [Dilip] WagesForAMonth

--- EmployeeWageComputationOops/Program.cs
+++ EmployeeWageComputationOops/Program.cs
@@ -9,29 +9,34 @@ namespace EmployeeWageComputationOops
 		const int WAGE_PER_HOUR = 20;
 		const int FULL_DAY_HOUR = 8;
 		const int PART_TIME_HOUR = 4;
+		const int MAX_WORKING_DAYS = 20;
 		public static void Main(String[] args)
 		{
 			Console.WriteLine("Welcome to employee wage computation!");
-
+			int monthlyWage = 0;
 			Random rand = new Random();
-			int attendance = rand.Next(0, 3);
-			int hoursWorked = 0;
-			int dailywage;
 
-			switch (attendance)
+			for (int i = 0; i < MAX_WORKING_DAYS; i++)
 			{
-				case EMP_FULL_TIME:
-					hoursWorked = FULL_DAY_HOUR;
-					break;
-				case EMP_PART_TIME:
-					hoursWorked = PART_TIME_HOUR;
-					break;
-				default:
-					break;
-			}//end Switch
+				int attendance = rand.Next(0, 3);
+				int hoursWorked = 0;
+				switch (attendance)
+				{
+					case EMP_FULL_TIME:
+						hoursWorked = FULL_DAY_HOUR;
+						break;
+					case EMP_PART_TIME:
+						hoursWorked = PART_TIME_HOUR;
+						break;
+					default:
+						break;
+				}//end Switch
 
-			dailywage = WAGE_PER_HOUR * hoursWorked;
-			Console.WriteLine($"Daily Wage : {dailywage}");
+				monthlyWage += WAGE_PER_HOUR * hoursWorked;
+			}
+
+
+			Console.WriteLine($"Monthly Wage : {monthlyWage}");
 		}
 	}
 }
